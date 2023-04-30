c9cf4d8b2b45ddf5d6a31294e5b94dae9881265c
toha2102 <toha2102@student.miun.se>
2023-05-01 00:40:24 +0200

    Like dislike implemented and roll

--- src/model/player.py
+++ src/model/player.py
@@ -14,3 +14,12 @@ class Player:
             self.disliked_characters = disliked_characters
         self.name = name
         self.score = 0
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name + " " + \
+            str(self.score) + " " + \
+            str(self.liked_characters) + " " + \
+            str(self.disliked_characters)
